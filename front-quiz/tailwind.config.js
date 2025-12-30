2c0bd9263db65e87b6ac9f3b14de9e4660506bf4
anton <antonrumancev67@gmail.com>
2025-12-30 22:40:55 +0300

    Created home page

--- front-quiz/tailwind.config.js
+++ front-quiz/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        myfont: ["Quicksand", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
